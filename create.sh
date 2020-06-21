3e7bdb29b1b1a2168acf6ccf038a3db09a68eaa9
Ganesha2282882 <sharmaganesha2@gmail.com>
2020-06-21 11:41:20 -0400

    Redesigned the floppies, formatted them

---
+++ create.sh
@@ -0,0 +1,52 @@
+# Part 1
+
+# Disk 1
+fallocate -l 1474560 1.img
+mkfs.vfat 1.img
+# Disk 2
+fallocate -l 1474560 2.img
+mkfs.vfat 2.img
+# Disk 3
+fallocate -l 1474560 3.img
+mkfs.vfat 3.img
+# Disk 4
+fallocate -l 1474560 4.img
+mkfs.vfat 4.img
+# Disk 5
+fallocate -l 1474560 5.img
+mkfs.vfat 5.img
+# Disk 6
+fallocate -l 1474560 6.img
+mkfs.vfat 6.img
+# Disk 7
+fallocate -l 1474560 7.img
+mkfs.vfat 7.img
+# Disk 8
+fallocate -l 1474560 8.img
+mkfs.vfat 8.img
+# Part 2
+
+# Disk 1
+fallocate -l 1474560 9.img
+mkfs.vfat 9.img
+# Disk 2
+fallocate -l 1474560 10.img
+mkfs.vfat 10.img
+# Disk 3
+fallocate -l 1474560 11.img
+mkfs.vfat 11.img
+# Disk 4
+fallocate -l 1474560 12.img
+mkfs.vfat 12.img
+# Disk 5
+fallocate -l 1474560 13.img
+mkfs.vfat 13.img
+# Disk 6
+fallocate -l 1474560 14.img
+mkfs.vfat 14.img
+# Disk 7
+fallocate -l 1474560 15.img
+mkfs.vfat 15.img
+# Disk 8
+fallocate -l 1474560 16.img
+mkfs.vfat 16.img
